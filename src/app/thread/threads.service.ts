c57609584c987315ffdac13312d93516b83e8e7a
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-10-05 19:13:02 +0300

    add orderedThreads and currentReadThreads

--- src/app/thread/threads.service.ts
+++ src/app/thread/threads.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+
 import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
-import * as _ from 'lodash';
 
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/combineLatest';
+
+import * as _ from 'lodash';
 
 import { Thread } from './thread.model';
 import { Message } from '../message/message.model';
@@ -12,7 +16,17 @@ import { MessagesService } from '../message/messages.service';
 
 @Injectable()
 export class ThreadsService {
+  // Observable that contains the most up to date list of threads
   threads: Observable<{ [key: string]: Thread }>;
+
+  // Contains the newest-first chronological list of threads
+  orderedThreads: Observable<Thread[]>;
+
+  // Contains the currently selected thread
+  currentThread: Subject<Thread> = new BehaviorSubject<Thread>(new Thread());
+
+  // Contains set of messages for the currently selected thread
+  currentThreadMessages: Observable<Message[]>;
 
   constructor(public messagesService: MessagesService) {
     this.threads = messagesService.messages.map((messages: Message[]) => {
@@ -35,5 +49,39 @@ export class ThreadsService {
 
       return threads;
     });
+
+    this.orderedThreads = this.threads.map(
+      (threadGroups: { [key: string]: Thread }) => {
+        const threads: Thread[] = _.values(threadGroups);
+        return _.sortBy(threads, (t: Thread) => t.lastMessage.sentAt).reverse();
+      }
+    );
+
+    this.currentThreadMessages = this.currentThread.combineLatest(
+      messagesService.messages,
+      (currentThread: Thread, messages: Message[]) => {
+        if (currentThread && messages.length > 0) {
+          return _.chain(messages)
+            .filter(
+              (message: Message) => message.thread.id === currentThread.id
+            )
+            .map((message: Message) => {
+              message.isRead = true;
+              return message;
+            })
+            .value();
+        } else {
+          return [];
+        }
+      }
+    );
+
+    this.currentThread.subscribe(this.messagesService.markThreadAsRead);
+  }
+
+  setCurrentThread(newThread: Thread): void {
+    this.currentThread.next(newThread);
   }
 }
+
+export const threadServiceInjectables: Array<any> = [ThreadsService];
